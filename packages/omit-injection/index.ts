d9add75a589223bdefa8efc5af4975f5fb9a6357
zyl <tinydustcode@163.com>
2022-11-01 10:56:46 +0800

    fix: injection refactor

--- packages/omit-injection/index.ts
+++ packages/omit-injection/index.ts
@@ -1,13 +1,4 @@
 export type {OmitModeTypes, OmitPaletteTypes, OmitFontPaletteTypes, OmitModePaletteTypes} from './types/palette';
-export type {
-    OmitBaseNodeTypes,
-    OmitBaseSizeTypes,
-    OmitBaseStylePropsTypes,
-    OmitBaseOptionBaseTypes,
-    OmitBaseStatusTypes,
-    OmitBaseStyleTypes
-} from './types/common';
-
 export {OmitPalette, OmitFontPalette} from './src/palette';
 export {OmitDefaultPrefixName, OmitDefaultPaletteMode} from './src/default';
 export {OmitPaletteInteractionRefer, OmitPaletteModeRefer} from './src/refer'
