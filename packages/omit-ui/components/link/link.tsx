da216f4b1dcff9227fe35fe6dcee6591197e11f6
YXR <913457727@qq.com>
2022-11-04 17:26:17 +0800

    feature: disable underline hover icon

--- packages/omit-ui/components/link/link.tsx
+++ packages/omit-ui/components/link/link.tsx
@@ -1,36 +1,52 @@
-import {FC, useMemo, useContext} from 'react'
-import {linkBaseProps} from "./type";
-import {ConfigContext} from "../../context/config-contenxt";
+import React, { FC, useMemo, useContext, ReactElement } from 'react';
+import { linkBaseProps } from './type';
+import { ConfigContext } from '../../context/config-contenxt';
 import { renderClassNames } from '../../utils/common';
 
-export const Link: FC<linkBaseProps> = (props) => {
-  const {prefixName} = useContext(ConfigContext)
+export const Link: FC<linkBaseProps> = props => {
+  const { prefixName } = useContext(ConfigContext);
   // init
   const {
     children,
     theme = 'default',
     size = 'medium',
-    underline = true
+    underline = false,
+    hover = 'underline',
+    disabled = false,
+    prefixIcon,
+    suffixIcon,
   } = props;
   const PrefixCName = `${prefixName}_link`;
+  const renderIcon = (icon?: ReactElement) => {
+    if (icon) {
+      return React.cloneElement(icon, {
+        className: `${PrefixCName}_icon`,
+      });
+    }
+    return null;
+  };
   const linkClass = useMemo(() => {
     const classNames = {
-        [`${PrefixCName}_${theme}`]: true,
-        [`${PrefixCName}_${size}`]: true,
-        [`${PrefixCName}_underline`]: !!underline
-    }
-    return renderClassNames(PrefixCName, classNames)
-  }, [])
+      [`${PrefixCName}_${theme}`]: true,
+      [`${PrefixCName}_${size}`]: true,
+      [`${PrefixCName}_underline`]: !!underline,
+      [`${PrefixCName}_disabled`]: !!disabled,
+      [`${PrefixCName}_hover_${hover}`]: true,
+      [`${PrefixCName}_prefixIcon`]: !!prefixIcon,
+      [`${PrefixCName}_suffixIcon`]: !!suffixIcon,
+    };
+    return renderClassNames(PrefixCName, classNames);
+  }, []);
 
   const renderLinkContent = useMemo(() => {
-    return children
-  }, [children])
+    return (
+      <>
+        {prefixIcon ? renderIcon(prefixIcon) : null}
+        {children}
+        {suffixIcon ? renderIcon(suffixIcon) : null}
+      </>
+    );
+  }, [children]);
 
-  return (
-    <a className={
-      linkClass
-    }>
-      {renderLinkContent}
-    </a>
-  )
-}
+  return <a className={linkClass}>{renderLinkContent}</a>;
+};
--- packages/omit-ui/components/link/type.ts
+++ packages/omit-ui/components/link/type.ts
@@ -1,18 +1,18 @@
-import {OmitBaseSizeTypes, OmitBaseNodeTypes, OmitBaseStyleTypes} from '../../types/common'
-
-import {OmitClickEvent} from "../../types/events";
-
-export type OmitButtonThemeTypes = 'default' | 'primary' | 'success' | 'warning' | 'danger'
-
+import { OmitBaseSizeTypes, OmitBaseNodeTypes, OmitBaseStyleTypes } from '../../types/common';
+import { OmitClickEvent } from '../../types/events';
+import { ReactElement } from 'react';
+export type OmitButtonThemeTypes = 'default' | 'primary' | 'success' | 'warning' | 'danger';
 export interface linkBaseProps {
   className?: string;
-    style?: OmitBaseStyleTypes;
-    theme?: OmitButtonThemeTypes
-    size?: OmitBaseSizeTypes;
-    loading?: boolean;
-    disabled?: boolean;
-    underline?: boolean;
-    // icon?: OmitBaseNodeTypes;
-    children?: OmitBaseNodeTypes;
-    onClick?: (event: OmitClickEvent) => void
+  style?: OmitBaseStyleTypes;
+  theme?: OmitButtonThemeTypes;
+  size?: OmitBaseSizeTypes;
+  loading?: boolean;
+  disabled?: boolean;
+  hover?: 'underline' | 'color';
+  underline?: boolean;
+  children?: OmitBaseNodeTypes;
+  prefixIcon?: ReactElement;
+  suffixIcon?: ReactElement;
+  onClick?: (event: OmitClickEvent) => void;
 }
