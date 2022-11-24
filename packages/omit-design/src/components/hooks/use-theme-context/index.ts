f533ee8c045da79abc5c42fcf641f8807151fa47
zyl <tinydustcode@163.com>
2022-11-24 15:39:41 +0800

    refactor: project refactor

--- packages/omit-design/src/components/hooks/use-theme-context/index.ts
+++ packages/omit-design/src/components/hooks/use-theme-context/index.ts
@@ -1,7 +1,10 @@
 import { useContext } from "react";
 import { ThemeContext } from "../../context";
 import { ThemeData, ThemeType } from "omit-theme";
-
+/*
+ * todo
+ *  合并theme数据到omit-theme提供的data中，再返回
+ * */
 export const useThemeContext = (): ThemeType => {
   const x = useContext(ThemeContext);
   console.log(x);
